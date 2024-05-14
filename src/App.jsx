d03382a8794e3be6708f44e0535722c6f36525c4
unknown <fasuarezd@gmail.com>
2024-05-13 20:58:09 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+import Saludo from './components/Saludo'
+import Cards from './components/Cards'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Saludo nombre={'Fabricio'} />
+      <Cards />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Saludo.jsx
@@ -0,0 +1,11 @@
+
+
+const Saludo = ({ nombre }) => {
+    return (
+        <>
+            <h1>Hola {nombre}</h1>
+        </>
+    )
+}
+
+export default Saludo
