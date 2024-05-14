291132a84d7450f9babd0e9a73879ce597b2799b
unknown <fasuarezd@gmail.com>
2024-05-13 21:17:22 -0600

    Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import Header from './components/Header'
 import Saludo from './components/Saludo'
 import Cards from './components/Cards'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import About from './pages/About'
+import Contacto from './pages/Contacto'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Saludo nombre={'Fabricio'} />
-      <Cards />
+      <Router>
+        <Header />
+        <nav>
+          <ul>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/contacto'>Contacto</Link>
+            </li>
+          </ul>
+        </nav>
+        <Saludo nombre={'Fabricio'} />
+        <Cards />
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/contacto' element={<Contacto />} />
+        </Routes>
+      </Router>
     </>
   )
 }
